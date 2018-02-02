ad1c899a6249fddb0533a4055b870b3727063f25
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-02-02 01:58:33 -0800

    fix: set version with ANYCLI_NEXT_VERSION

--- src/commands/manifest.ts
+++ src/commands/manifest.ts
@@ -1,9 +1,13 @@
-import {Command, parse} from '@anycli/command'
+import {Command, flags, parse} from '@anycli/command'
 import cli from 'cli-ux'
+import * as fs from 'fs-extra'
 import * as path from 'path'
 
 export default class Manifest extends Command {
   static title = 'generates plugin manifest json'
+  static flags = {
+    out: flags.string({char: 'o', description: 'output to file at path'}),
+  }
   static args = [
     {name: 'path', description: 'path to plugin', default: '.'}
   ]
@@ -14,6 +18,13 @@ export default class Manifest extends Command {
     const root = path.resolve(this.options.args.path)
     const plugin = await this.config.engine.loadPlugin({root, type: 'dev'})
     if (!plugin) throw new Error(`${this.config.name} plugin not found`)
-    cli.styledJSON(plugin.manifest)
+    if (process.env.ANYCLI_NEXT_VERSION) {
+      plugin.manifest.version = process.env.ANYCLI_NEXT_VERSION
+    }
+    if (this.options.flags.out) {
+      await fs.outputJSON(this.options.flags.out, plugin)
+    } else {
+      cli.styledJSON(plugin.manifest)
+    }
   }
 }
