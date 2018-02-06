63f1385d740d8d201bbda1effd4657eb15a69e07
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-02-06 15:29:47 -0800

    feat: added multi readme

--- src/commands/readme.ts
+++ src/commands/readme.ts
@@ -1,101 +1,172 @@
 // tslint:disable no-implicit-dependencies
 
-import {Command} from '@anycli/command'
+import {Command, flags} from '@anycli/command'
 import * as Config from '@anycli/config'
 import Help from '@anycli/plugin-help'
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
-import {compact, sortBy, uniqBy} from '../util'
+import {compact, sortBy, template, uniqBy} from '../util'
 
 const normalize = require('normalize-package-data')
 
 export default class Readme extends Command {
   static description = 'adds commands to readme'
+  static flags = {
+    multi: flags.boolean({description: 'create a different markdown page for each topic'})
+  }
 
   static aliases = ['c', 'd']
 
   async run() {
-    const config = Config.load({root: process.cwd()})
+    const {flags} = this.parse(Readme)
+    const config = Config.load({root: process.cwd(), devPlugins: false, userPlugins: false})
     try {
       let p = require.resolve('@anycli/plugin-legacy')
       config.plugins.push(new Config.Plugin({root: p}))
     } catch {}
     await config.runHook('init', {id: 'readme', argv: this.argv})
     let readme = await fs.readFile('README.md', 'utf8')
+    let commands = config.commands
+    commands = commands.filter(c => !c.hidden)
+    commands = commands.filter(c => c.pluginType === 'core')
+    commands = uniqBy(commands, c => c.id)
+    commands = sortBy(commands, c => c.id)
     // if (readme.includes('<!-- toc -->')) {
     // } else this.warn('<!-- toc --> not found in README')
-    readme = this.commands(config, readme)
+    readme = this.replaceTag(readme, 'install', this.install(config))
+    readme = this.replaceTag(readme, 'usage', this.usage(config))
+    readme = this.replaceTag(readme, 'commands', flags.multi ? this.multiCommands(config, commands) : this.commands(config, commands))
 
-    console.log(readme)
+    readme = readme.trimRight()
+    readme += '\n'
+    await fs.outputFile('README.md', readme)
   }
 
-  commands(config: Config.IConfig, readme: string): string {
-    const help = new Help(config, {stripAnsi: true, maxWidth: 120})
-    const build = (): string => {
-      let commands = this.config.commands
-      commands = commands.filter(c => !c.hidden)
-      commands = uniqBy(commands, c => c.id)
-      commands = sortBy(commands, c => c.id)
-      const renderCommand = (level: number) => (c: Config.Command): string => {
-        const header = () => '#'.repeat(level) + ` ${c.id}`
-        const code = () => {
-          let pluginName = c.pluginName
-          if (!pluginName) return
-          let plugin = config.plugins.concat([config]).find(p => p.name === c.pluginName)
-          if (!plugin) return
-          normalize(plugin.pjson)
-          let repo = plugin.pjson.repository
-          let commandsDir = plugin.pjson.anycli.commands
-          if (!repo || !repo.url || !commandsDir) return
-          commandsDir = commandsDir.replace(/\.\//, '')
-          if (plugin.name === this.config.name) pluginName = path.join(__dirname, '../..')
-          let commandPath = require.resolve(`${pluginName}/${commandsDir}/${c.id.replace(/:/g, '/')}`)
-          commandPath = commandPath.replace(path.dirname(require.resolve(`${pluginName}/package.json`)) + '/', '')
-          if (plugin.pjson.devDependencies.typescript) {
-            commandPath = commandPath.replace(/^lib\//, 'src/')
-            commandPath = commandPath.replace(/\.js$/, '.ts')
-          }
-          repo = repo.url.split('+')[1].replace(/\.git$/, '')
-          return `_See code: [${plugin.name}](${repo}/blob/master/${commandPath})_`
-          // return `_From plugin: [${plugin.name}](${plugin.pjson.homepage})_`
-        }
-        const subcommands = (): string | undefined => {
-          return commands
-          .filter(sc => sc.id.startsWith(c.id) && sc.id !== c.id)
-          .map(renderCommand(level + 1))
-          .map(c => c.trim())
-          .join('\n\n')
-        }
-        return compact([
-          header(),
-          '```\n' + help.command(c).trim() + '\n```',
-          code(),
-          subcommands(),
-        ]).join('\n\n')
+  replaceTag(readme: string, tag: string, body: string): string {
+    if (readme.includes(`<!-- ${tag} -->`)) {
+      if (readme.includes(`<!-- ${tag}stop -->`)) {
+        readme = readme.replace(new RegExp(`<!-- ${tag} -->(.|\n)*<!-- ${tag}stop -->`, 'm'), `<!-- ${tag} -->`)
       }
-      let rootCommands = commands.filter(c => !c.id.includes(':'))
-
-      return [
-        '<!-- commands -->',
-        '# Commands\n',
-        ...commands.map(c => {
-          return `* [${c.id}](#${c.id.replace(/:/g, '')})`
-        }),
-        ...rootCommands.map(renderCommand(2)).map(s => s.trim() + '\n'),
-        '<!-- commandsstop -->',
-      ].join('\n').trim()
-    }
-    if (readme.includes('<!-- commands -->')) {
-      if (readme.includes('<!-- commandsstop -->')) {
-        // clear out current commands
-        readme = readme.replace(/<!-- commands -->(.|\n)*<!-- commandsstop -->/m, '<!-- commands -->')
-      }
-      readme = readme.replace(/<!-- commands -->/, build())
     } else {
-      // add commands to end
-      readme += `\n${build()}`
+      readme += `\n<!-- ${tag} -->`
     }
-    return readme
+    return readme.replace(`<!-- ${tag} -->`, `<!-- ${tag} -->\n${body}\n<!-- ${tag}stop -->`)
+  }
+
+  install(config: Config.IConfig): string {
+    return [
+        `# Installing ${config.name}\n`,
+        'with yarn:',
+        '```\n$ yarn global add ' + config.name + '\n```\n',
+        'or with npm:',
+        '```\n$ npm install -g ' + config.name + '\n```\n',
+    ].join('\n').trim()
+  }
+
+  usage(config: Config.IConfig): string {
+      return [
+        '# Usage\n',
+        `\`\`\`sh-session
+$ ${config.bin} COMMAND
+running command...
+$ ${config.bin} (-v|--version|version)
+${config.name}/${config.version} (${process.platform}-${process.arch}) node-v${process.versions.node}
+$ ${config.bin} --help [COMMAND]
+USAGE
+  $ ${config.bin} COMMAND [OPTIONS]
+...
+\`\`\`\n`,
+      ].join('\n').trim()
+  }
+
+  multiCommands(config: Config.IConfig, commands: Config.Command[]): string {
+    let topics = config.topics
+    topics = topics.filter(t => !t.hidden)
+    topics = topics.filter(t => commands.find(c => c.id.startsWith(t.name)))
+    topics = sortBy(topics, t => t.name)
+    topics = uniqBy(topics, t => t.name)
+    for (let topic of topics) {
+      this.createTopicFile(
+        path.join('.', 'docs', topic.name.replace(/:/g, '/') + '.md'),
+        config,
+        topic,
+        commands.filter(c => c.id.startsWith(topic.name)),
+      )
+    }
+
+    return [
+      '# Command Topics\n',
+      ...topics.map(t => {
+        return compact([
+          `* [${config.bin} ${t.name}](docs/${t.name.replace(/:/g, '/')}.md)`,
+          t.description,
+        ]).join(' - ')
+      }),
+    ].join('\n').trim() + '\n'
+  }
+
+  createTopicFile(file: string, config: Config.IConfig, topic: Config.Topic, commands: Config.Command[]) {
+    let doc = [
+      `${config.bin} ${topic.name}`,
+      '='.repeat(`${config.bin} ${topic.name}`.length),
+      '',
+      config.pjson.anycli.description || config.pjson.description,
+      this.commands(config, commands),
+    ].join('\n').trim() + '\n'
+    fs.outputFileSync(file, doc)
+  }
+
+  commands(config: Config.IConfig, commands: Config.Command[]): string {
+    let rootCommands = commands.filter(c => !c.id.includes(':'))
+    return [
+      '# Commands\n',
+      ...commands.map(c => {
+        return `* [${config.bin} ${c.id}](#${c.id.replace(/:/g, '')})`
+      }),
+      ...rootCommands.map(c => this.renderCommand(config, c, commands)).map(s => s.trim() + '\n'),
+    ].join('\n').trim()
+  }
+
+  renderCommand(config: Config.IConfig, c: Config.Command, commands: Config.Command[], level: number = 2): string {
+    let title = template({config})(c.description || '').split('\n')[0]
+    const help = new Help(config, {stripAnsi: true, maxWidth: 120})
+    const header = () => '#'.repeat(level) + ` ${c.id}`
+    const subcommands = (): string | undefined => {
+      return commands
+      .filter(sc => sc.id.startsWith(c.id) && sc.id !== c.id)
+      .map(c => this.renderCommand(config, c, commands, level + 1))
+      .map(c => c.trim())
+      .join('\n\n')
+    }
+    return compact([
+      header(),
+      title,
+      '```\n' + help.command(c).trim() + '\n```',
+      this.commandCode(config, c),
+      subcommands(),
+    ]).join('\n\n')
+  }
+
+  commandCode(config: Config.IConfig, c: Config.Command): string | undefined {
+    let pluginName = c.pluginName
+    if (!pluginName) return
+    let plugin = config.plugins.concat([config]).find(p => p.name === c.pluginName)
+    if (!plugin) return
+    normalize(plugin.pjson)
+    let repo = plugin.pjson.repository
+    let commandsDir = plugin.pjson.anycli.commands
+    if (!repo || !repo.url || !commandsDir) return
+    commandsDir = commandsDir.replace(/\.\//, '')
+    if (plugin.name === config.name) pluginName = path.join(__dirname, '../..')
+    let commandPath = require.resolve(`${pluginName}/${commandsDir}/${c.id.replace(/:/g, '/')}`)
+    commandPath = commandPath.replace(path.dirname(require.resolve(`${pluginName}/package.json`)) + '/', '')
+    if (plugin.pjson.devDependencies.typescript) {
+      commandPath = commandPath.replace(/^lib\//, 'src/')
+      commandPath = commandPath.replace(/\.js$/, '.ts')
+    }
+    repo = repo.url.split('+')[1].replace(/\.git$/, '')
+    return `_See code: [${plugin.name}](${repo}/blob/master/${commandPath})_`
+    // return `_From plugin: [${plugin.name}](${plugin.pjson.homepage})_`
   }
 }
