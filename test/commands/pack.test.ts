a037cfe9150d0c6969b5924846fa4857fcafb2b1
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-07 03:06:54 -0700

    fix: tests

--- test/commands/pack.test.ts
+++ test/commands/pack.test.ts
@@ -7,13 +7,13 @@ describe('pack', () => {
   })
 
   test
-  .command(['pack', '-pwin32', '-ax64', '-cdev', '-otmp/tarballs/win32-x64.tar.gz'])
+  .command(['pack', '-pwin32', '-ax64', '-cdev', '-otmp/tarballs/win32-x64'])
   .it('packs win32-64', () => {
     expect(qq.exists.sync('tmp/tarballs/win32-x64.tar.gz')).to.be.true
   })
 
   test
-  .command(['pack', '-plinux', '-ax86', '-cdev', '-otmp/tarballs/linux-x86.tar.gz'])
+  .command(['pack', '-plinux', '-ax86', '-cdev', '-otmp/tarballs/linux-x86'])
   .it('packs linux-x86', () => {
     expect(qq.exists.sync('tmp/tarballs/linux-x86.tar.gz')).to.be.true
   })
