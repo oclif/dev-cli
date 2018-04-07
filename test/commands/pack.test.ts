14dd2fe5087f9027388a8b90ab298587f95d053c
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-07 12:36:44 -0700

    fix: skip pack test on windows

--- test/commands/pack.test.ts
+++ test/commands/pack.test.ts
@@ -3,12 +3,14 @@ import * as qq from 'qqjs'
 
 const {version} = require('../../package.json')
 
+const skipIfWindows = process.platform === 'win32' ? test.skip() : test
+
 describe('pack', () => {
   beforeEach(async () => {
     await qq.rm('dist')
   })
 
-  test
+  skipIfWindows
   .command(['pack'])
   .it('packs win32-64', () => {
     expect(qq.exists.sync(`dist/oclif-dev-v${version}.tar.gz`)).to.be.true
