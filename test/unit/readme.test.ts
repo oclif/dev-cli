3dfabc81607f39f05046b298811b392047d2c841
Thomas <t.dvornik@salesforce.com>
2020-04-30 11:32:52 -0700

    test: add some tests for command format

--- test/unit/readme.test.ts
+++ test/unit/readme.test.ts
@@ -5,19 +5,38 @@ const readme = fs.readFileSync('README.md', 'utf8')
 
 describe('readme', () => {
   test
-  .stdout()
-  .finally(() => fs.writeFile('README.md', readme))
-  .command(['readme'])
-  .it('runs readme', () => {
-    expect(fs.readFileSync('README.md', 'utf8')).to.contain('manifest')
-  })
+    .stdout()
+    .finally(() => fs.writeFile('README.md', readme))
+    .command(['readme'])
+    .it('runs readme', () => {
+      expect(fs.readFileSync('README.md', 'utf8')).to.contain('manifest')
+    })
 
   test
-  .stdout()
-  .finally(() => fs.writeFile('README.md', readme))
-  .finally(() => fs.remove('docs'))
-  .command(['readme', '--multi'])
-  .it('runs readme --multi', () => {
-    expect(fs.readFileSync('README.md', 'utf8')).to.contain('manifest')
-  })
+    .stdout()
+    .finally(() => fs.writeFile('README.md', readme))
+    .finally(() => fs.remove('docs'))
+    .command(['readme', '--multi'])
+    .it('runs readme --multi', () => {
+      expect(fs.readFileSync('README.md', 'utf8')).to.contain('manifest')
+    })
+
+  test
+    .stdout()
+    .finally(() => fs.writeFile('README.md', readme))
+    .command(['readme', '-f', "'<%= command.id %>'"])
+    .it('runs readme -f \'<%= command.id %>\'', () => {
+      const readme = fs.readFileSync('README.md', 'utf8').split('\n')
+      expect(readme.find(line => line.startsWith("* [`'readme'`](#readme)"))).to.exist // No bin name
+    })
+
+  test
+    .stdout()
+    .do(() => fs.writeFile('README.md', readme.replace('# Commands', '## Commands')))
+    .finally(() => fs.writeFile('README.md', readme))
+    .command(['readme'])
+    .it('runs readme subheader markers', () => {
+      const readme = fs.readFileSync('README.md', 'utf8').split('\n')
+      expect(readme.find(line => line.startsWith('### `oclif-dev readme`'))).to.exist
+    })
 })
