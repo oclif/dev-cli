85745f24d6e4adf2fd33225987fda3822fd44e7f
Jamie White <jameswhite@salesforce.com>
2020-09-07 19:04:36 +0200

    Add test for readme-generation with custom help
    Co-authored-by: Chad Carbert <chadcarbert@me.com>

--- test/readme.test.ts
+++ test/readme.test.ts
@@ -1,5 +1,6 @@
 import {expect, test} from '@oclif/test'
 import * as fs from 'fs-extra'
+import * as path from 'path'
 
 const readme = fs.readFileSync('README.md', 'utf8')
 
@@ -19,5 +20,22 @@ describe('readme', () => {
   .command(['readme', '--multi'])
   .it('runs readme --multi', () => {
     expect(fs.readFileSync('README.md', 'utf8')).to.contain('manifest')
+  })
+
+  describe('with custom help', () => {
+    const rootPath = path.join(__dirname, 'fixtures/cli-with-custom-help')
+    const readmePath = path.join(rootPath, 'README.md')
+    const originalReadme = fs.readFileSync(readmePath, 'utf8')
+
+    test
+    .stdout()
+    .finally(() => fs.writeFileSync(readmePath, originalReadme))
+    .stub(process, 'cwd', () => rootPath)
+    .command(['readme'])
+    .it('prints custom help to the readme', () => {
+      const newReadme = fs.readFileSync(readmePath, 'utf8')
+
+      expect(newReadme).to.contain('Custom help for hello')
+    })
   })
 })
